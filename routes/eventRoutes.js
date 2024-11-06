8b0450b450e6315bae50f162cf3aedb721c07a79
Mounika-github99 <mounikabandlamudi7@gmail.com>
2024-11-06 19:30:45 +0530

    mern

---
+++ routes/eventRoutes.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const Event = require('../models/Event');
+
+// Create a new event
+router.post('/', async (req, res) => {
+    const newEvent = new Event(req.body);
+    try {
+        const savedEvent = await newEvent.save();
+        res.status(201).json(savedEvent);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get all events
+router.get('/', async (req, res) => {
+    try {
+        const events = await Event.find();
+        res.status(200).json(events);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Get an event by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const event = await Event.findById(req.params.id);
+        res.status(200).json(event);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Update an event
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedEvent = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.status(200).json(updatedEvent);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Delete an event
+router.delete('/:id', async (req, res) => {
+    try {
+        await Event.findByIdAndDelete(req.params.id);
+        res.status(200).json("Event deleted");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
